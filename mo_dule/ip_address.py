4f5f8b3f225516e0cbd6813da7a7e5e43f6e38a0
kuiooo00 <lkheh2517@gmail.com>
2016-01-04 16:53:20 +0800

    demo module

---
+++ mo_dule/ip_address.py
@@ -0,0 +1,13 @@
+import os
+
+def get_ip_address(url):
+
+    command = "host " + url
+    process = os.popen(command)
+    results = str(process.read())
+
+    marker = results.find('has address') + 12
+
+    return results[marker:].splitlines()[0]
+
+get_ip_address("www.baidu.com")
---
+++ mo_dule/nmap.py
@@ -0,0 +1,14 @@
+import os
+
+def get_nmap(options,ip):
+
+    command = "nmap " + options + "  "+ ip
+    process = os.popen(command)
+
+    results = str(process.read())
+
+    return results
+
+print('Nmap scanning finished')
+
+
